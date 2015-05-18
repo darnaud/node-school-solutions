4cabf7c6c9daecaff91b1dff8d3853d60729cee3
darnaud <twists@live.fr>
2015-05-18 10:38:02 -0500

    added comments to solution 3

--- learnyounode/my-first-io.js
+++ learnyounode/my-first-io.js
@@ -1,6 +1,15 @@
-var filepath = process.argv[2] ;
-var fs = require('fs') ;
+var filepath = process.argv[2] ; 
+var fs = require('fs') ; // filesystem operation module
 
+
+//File given by "filepath" is read, converted 
+//to string and stored into "buffer" variable
 var buffer = fs.readFileSync(filepath).toString() ;
 
-console.log(buffer);
+
+// buffer string is split into array of 
+//substrings with "\n" as delimiter
+// then length of said array is returned
+//we add " - 1" because there is no newline 
+//character at then of testfile
+console.log(buffer.split('\n').length + 1);
