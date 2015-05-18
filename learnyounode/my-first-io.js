8dad4b3e4012464b675ea23db5f7294f3e9e8b1f
darnaud <twists@live.fr>
2015-05-18 10:57:05 -0500

    Corrected solution to exercise 3

--- learnyounode/my-first-io.js
+++ learnyounode/my-first-io.js
@@ -2,14 +2,15 @@ var filepath = process.argv[2] ;
 var fs = require('fs') ; // filesystem operation module
 
 
-//File given by "filepath" is read, converted 
-//to string and stored into "buffer" variable
-var buffer = fs.readFileSync(filepath).toString() ;
+console.log(fs.readFileSync(filepath).toString().split('\n').length - 1);
 
 
-// buffer string is split into array of 
-//substrings with "\n" as delimiter
-// then length of said array is returned
-//we add " - 1" because there is no newline 
-//character at then of testfile
-console.log(buffer.split('\n').length + 1);
+//Note//
+
+//this is the code equivalent of a run-on sentence :D
+//the file given by "filepath" :
+//is read, 
+//converted to string,
+//split into array of substrings with '\n' delimiter,
+//then 1 is substracted from form the arrray length and Voila
+//We got the number of newlines in the input file
