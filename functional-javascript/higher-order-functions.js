2f2512c901d26ac0b3cb7508ba01a7d2ebaee150
darnaud <twists@live.fr>
2015-05-26 02:32:43 -0500

    updated functional-javascript

---
+++ functional-javascript/higher-order-functions.js
@@ -0,0 +1,22 @@
+function repeat(operation, num) {
+      	
+      	for (var i =0; i < num; i++)
+      		operation();
+    
+    }
+    module.exports = repeat
+
+    /*
+
+    Official solution
+
+    function repeat(operation, num) {
+      if (num <= 0) return
+      operation()
+      return repeat(operation, --num)
+    }
+    
+    module.exports = repeat
+    */
+
+    
