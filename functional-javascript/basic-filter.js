5f6a2e72a902a23c4f6de31303239ca64022e3b6
darnaud <twists@live.fr>
2015-05-28 19:38:19 -0500

    exercise 4 and 5 done with help

--- functional-javascript/basic-filter.js
+++ functional-javascript/basic-filter.js
@@ -1,10 +1,30 @@
 module.exports = function getShortMessages(messages){
 
-	//
+	var k = 0;
 
-	return messages.filter(function(obj){
+	var arr = {}
 
-		var arrayOfMessages =[];
+	arr = messages.filter(function(obj){
+
 		return (obj.message.length < 50)
+
 	})
+
+	return arr.map(function(obj){
+		 
+		 return obj.message 
+		 
+	})
+
 }
+
+/* Official Solution
+
+module.exports = function getShortMessages(messages) {
+      return messages.filter(function(item) {
+        return item.message.length < 50
+      }).map(function(item) {
+        return item.message
+      })
+    }
+*/
