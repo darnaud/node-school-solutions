2870460e604e32ea790c7d0c5fd0ff1b22ae5276
darnaud <twists@live.fr>
2015-05-30 15:32:48 -0500

    time-server

--- learnyounode/time-server.js
+++ learnyounode/time-server.js
@@ -1,20 +1,48 @@
 var net = require('net');
+
 var portNo = process.argv[2];
-var date = new Date()
 
+function pad(num) {
+    return (num < 10 ? '0' : '') + num;
+}
 
+var server = net.createServer( function(socket){
 
-var server = net.createServer(function(socket){
-	
-	socket.end(String(date.getFullYear() +'-'))
+    var date = new Date()
+    socket.end(
+
+          pad(date.getFullYear()) + '-'
+        + pad((date.getMonth()+1)) +'-'
+        + pad(date.getDate()) + ' '
+        + pad(date.getHours()) + ':'
+        + pad(date.getMinutes())+ '\n');
 })
 
-server.listen(portNo)
 
- //ls"YYYY-MM-DD hh:mm"
+server.listen(portNo);
 
- date.getFullYear()
-    date.getMonth()     // starts at 0
-    date.getDate()      // returns the day of month
-    date.getHours()
-    date.getMinutes()
+
+/* Official Solution
+
+var net = require('net')
+    
+    function zeroFill(i) {
+      return (i < 10 ? '0' : '') + i
+    }
+    
+    function now () {
+      var d = new Date()
+      return d.getFullYear() + '-'
+        + zeroFill(d.getMonth() + 1) + '-'
+        + zeroFill(d.getDate()) + ' '
+        + zeroFill(d.getHours()) + ':'
+        + zeroFill(d.getMinutes())
+    }
+    
+    var server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+*/
